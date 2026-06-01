a2a07c8c99ea025737f3cbad8078f07fb4ba3186
adebayo adebajo <bayoradebajo@gmail.com>
2026-06-01 10:11:20 +0100

    form section initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // import { useState } from 'react'
 import './App.css'
+import FormSection from './components/FormSection'
 
 function App() {
 
 
   return (
     <>
-      
+      <FormSection></FormSection>
     </>
   )
 }
