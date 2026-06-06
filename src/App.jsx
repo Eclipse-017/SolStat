a5d1043bcfad42deb988a1508e3581caf1a4f65c
Richard Okafor <javadevrekahdo@gmail.com>
2026-06-06 10:37:04 +0100

    save progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import './styles/variables.css'
 import './styles/reset.css'
 import './styles/utilities.css'
 import './styles/queries.css'
+
 import VideoSection from './components/VideoSection/VideoSection'
 import HeroSection from './components/HeroSection/HeroSection'
-
-import VideoSection from './components/VideoSection/VideoSection'
 import ImageSection from './components/imageSection/ImageSection'
-import './components/imageSection/imageSection.css'
-
 import TableSection from './components/TableSection/TableSection'
 
 function App() {
