5b7e71c84e0d5224218352c6235834caa5b82ff6
Chioma Okpor <estee.ojumah@gmail.com>
2026-06-11 08:51:40 +0100

    Added planet name and distance tags to planet card

--- src/components/PlanetCard/PlanetCard.jsx
+++ src/components/PlanetCard/PlanetCard.jsx
@@ -11,8 +11,15 @@ function PlanetCard({className, src, alt, name, index, distance}){
                 </div>
 
                 <figcaption className="planet-info">
-                    <h3 className='planet-name'>{name}</h3>
-                    <p className='planet-distance'>{distance}</p>
+                    <div className='planet-info-group'>
+                        <span className='planet-info-tag'>Planet Name</span>
+                        <h3 className='planet-name'>{name}</h3>
+                    </div>
+                     
+                    <div className='planet-info-group'>
+                        <span className='planet-info-tag'>Distance From Sun</span>
+                        <p className='planet-distance'>{distance}</p>
+                    </div>
                 </figcaption>
             </figure>
         </div>
