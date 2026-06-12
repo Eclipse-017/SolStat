f071499fed82e5f2ab68823ca4baaa0ab6488a3d
Chioma Okpor <estee.ojumah@gmail.com>
2026-06-12 14:47:20 +0100

    made some changes in the styles

--- src/sections/FooterSection/FooterSection.jsx
+++ src/sections/FooterSection/FooterSection.jsx
@@ -1,73 +1,81 @@
 import "./FooterSection.css";
+import MainSection from '../../components/MainSection/MainSection'
 
 const FooterSection = () => {
   const students = [
     {
       name: "Abdulsalam",
-      github: "",
+      github: "https://github.com/Eclipse-017",
     },
     {
       name: "Esther",
-      github: "",
+      github: "https://github.com/Estimilita",
     },
     {
       name: "Richard",
-      github: "",
+      github: "https://github.com/Rekahdo",
     },
     {
       name: "Silver",
-      github: "",
+      github: "https://github.com/silver-orioha",
     },
     {
-      name: "Abayomi",
-      github: "",
+      name: "Adebayo",
+      github: "https://github.com/bayoradebajo",
     },
     {
       name: "Tosin",
-      github: "",
+      github: "https://github.com/tosinabdul2-sudo",
     },
   ];
 
   return (
     <>
       <footer className="footer-section">
-        <div>
-          <h4 className="heading">About</h4>
-
-          <p className="heading-description">
-            The group members who worked on this project are:
-          </p>
-
-          <nav className="students-nav">
-            <ul className="student-links">
-                {
-                    students.map(({name, github}, index) => (
-                        <li className="student-link" key={index}><a href={github}>{name}</a></li>
-                    ))
-                }
-            </ul>
-          </nav>
-
-          <hr className="line" />
+        <MainSection>
           <div>
-            <p className="about-info">
-              &#169; 2026 Designed by{" "}
-              <a href="https://amakandukwu.com/">Amaka </a> &{" "}
-              <a href="https://www.linkedin.com/in/ifeomaokocha">Ifeoma A.</a>
+            <h4 className="heading">About</h4>
+
+            <p className="heading-description">
+              The group members who worked on this project are:
             </p>
+
+            <nav className="students-nav">
+              <ul className="student-links">
+                  {
+                      students.map(({name, github}, index) => (
+                          <li className="student-link" key={index}><a href={github} target="_blank">{name}</a></li>
+                      ))
+                  }
+              </ul>
+            </nav>
           </div>
-          <div className="container">
+
+          <div>
+            <hr className="line" />
+
             <div>
-              <span>Built by Phoenix Group 26.</span>
-              <span id="copyright">All rights reserved</span>
+              <p className="about-info">
+                &#169; 2026 Designed by{" "}
+                <a href="https://amakandukwu.com/">Amaka </a> &{" "}
+                <a href="https://www.linkedin.com/in/ifeomaokocha">Ifeoma A.</a>
+              </p>
             </div>
-            <div>
-              <a href="https://tsacademyonline.com/" className="ts-link">
-                TSAcademy
-              </a>
+
+            <div className="container">
+              <div>
+                <span>Built by Phoenix Group 26. </span>
+                <span id="copyright">All rights reserved</span>
+              </div>
+              <div>
+                <a href="https://tsacademyonline.com/" target="_blank" className="ts-link">
+                  TSAcademy
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+
+        </MainSection>
       </footer>
     </>
   );
