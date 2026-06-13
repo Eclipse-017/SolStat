9121129aac3c01121607b6f06f0ba45be96615d0
Eclipse-017 <ajibadeabdulsalam42@gmail.com>
2026-06-13 10:47:05 +0100

    Added new css file containing responsiveness in the hero sectiion

--- src/sections/HeroSection/HeroSection.jsx
+++ src/sections/HeroSection/HeroSection.jsx
@@ -1,43 +1,44 @@
 import './HeroSection.css';
-import Button from '../../components/Button/Button'
+import MainSection from '../../components/MainSection/MainSection'
 
 function HeroSection() {
   return (
-    <>  
-      {/* HEADER */}
-      <header>
-        <nav className="navigation">
-          <div className="logo">
-            <img src="src/assets/graphics.png" alt="Logo" /> 
-          </div>
+    <MainSection className="hero-section">  
+      <header className='hero-header'>
+        <nav>
+          <a href="logo-container">
+            <img src="src/assets/graphics.png" alt="Logo" />
+          </a>
         </nav>
       </header>
 
-      {/* HERO SECTION */}
-        <section className="hero-section">
+      <section className="hero-content">
 
-          <div className="hero-content">
-            <h1>
-              Explore Our Solar System Through Data
-            </h1>
-            <p>
-              Understand the planets not just by name, but by measurable
-              facts. From size and mass to gravity and density, this page
-              breaks down the solar system in a clear, data-driven way.
-            </p>
+        <div className="hero-info">
 
-            <div className="buttons">
-              <Button> <a href="#image-section-id">Explore the Data</a></Button>
-              <Button className='btn-alt'> <a href="#form-section-id">Contact Us</a></Button>
-            </div>
+          <h1>
+            Explore Our Solar System Through Data
+          </h1>
+
+          <p>
+            Understand the planets not just by name, but by measurable
+            facts. From size and mass to gravity and density, this page
+            breaks down the solar system in a clear, data-driven way.
+          </p>
+
+          <div className="buttons">
+            <a href="#image-section-id">Explore the Data</a>
+            <a className='btn-alt' href="#form-section-id">Contact Us</a>
           </div>
 
-          <div className="hero-image">
-            <img src="src/assets/heroimage.png" alt="The Blue Earth" /> 
-          </div>
+        </div>
 
-        </section>
-    </> );           
+        <div className="hero-image">
+            <img src="src\assets\heroimage.png" alt="The Blue Earth" width="100px"/> 
+        </div>
+
+      </section>
+    </MainSection> );           
 }
 
 export default HeroSection;
