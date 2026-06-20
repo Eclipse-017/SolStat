a701f39b2de77163bc236d63ebc40e779e2bf359
Eclipse-017 <ajibadeabdulsalam42@gmail.com>
2026-06-20 14:12:54 +0100

    Reaolve Deployment Issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,15 @@ import './styles/reset.css'
 import './styles/utilities.css'
 import './styles/queries.css'
 
-import HeroSection from './sections/HeroSection/HeroSection'
-import ImageSection from './sections/ImageSection/ImageSection'
-import TableSection from './sections/TableSection/TableSection'
-import FormSection from './sections/FormSection/FormSection'
-import VideoSection from './sections/VideoSection/VideoSection'
-import FooterSection from './sections/FooterSection/FooterSection'
 
-import solStatVideo from './assets/video.mp4';
+import VideoSection from './sections/VideoSection/VideoSection'
 
 function App() {
   return (
     <>
-      <HeroSection />
-
       <main>
         <VideoSection src={solStatVideo}/>
-        <ImageSection />
-        <TableSection />
-        <FormSection />
       </main>
-
-       <FooterSection />
     </>
   )
 }
