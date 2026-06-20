a9fc9d8005643af8fdc8a9de5a239f689308d6b5
Eclipse-017 <ajibadeabdulsalam42@gmail.com>
2026-06-20 14:16:56 +0100

    Resolve Deployment Issues

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,14 @@ import './styles/reset.css'
 import './styles/utilities.css'
 import './styles/queries.css'
 
-import HeroSection from './sections/HeroSection/HeroSection'
+
 import ImageSection from './sections/ImageSection/ImageSection'
-import TableSection from './sections/TableSection/TableSection'
-import FormSection from './sections/FormSection/FormSection'
-import VideoSection from './sections/VideoSection/VideoSection'
-import FooterSection from './sections/FooterSection/FooterSection'
-
-import solStatVideo from './assets/video.mp4';
-
 function App() {
   return (
     <>
-      <HeroSection />
-
       <main>
-        <VideoSection src={solStatVideo}/>
         <ImageSection />
-        <TableSection />
-        <FormSection />
       </main>
-
-       <FooterSection />
     </>
   )
 }
